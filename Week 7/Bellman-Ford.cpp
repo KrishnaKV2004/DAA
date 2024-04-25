e405f6460863e633f5f86db414bc66a14cb9555a
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2024-04-25 09:14:38 +0530

    Updated

--- Week 7/Bellman-Ford.cpp
+++ Week 7/Bellman-Ford.cpp
@@ -5,35 +5,44 @@ using namespace std;
 
 #define INF INT_MAX
 
-struct Edge {
+struct Edge
+{
     int src, dest, weight;
 };
 
-void bellmanFord(const vector<Edge>& edges, int V, int source) {
+void bellmanFord(const vector<Edge>& edges, int V, int source)
+{
     vector<int> dist(V, INF);
     dist[source] = 0;
 
-    for (int i = 0; i < V - 1; ++i) {
-        for (const Edge& edge : edges) {
-            if (dist[edge.src] != INF && dist[edge.src] + edge.weight < dist[edge.dest]) {
+    for (int i = 0; i < V - 1; ++i)
+    {
+        for (const Edge& edge : edges)
+        {
+            if (dist[edge.src] != INF && dist[edge.src] + edge.weight < dist[edge.dest])
+            {
                 dist[edge.dest] = dist[edge.src] + edge.weight;
             }
         }
     }
 
-    for (int i = 0; i < V; ++i) {
+    for (int i = 0; i < V; ++i)
+    {
         cout << i + 1 << " : " << dist[i] << endl;
     }
 }
 
-int main() {
+int main()
+{
     int V;
     cin >> V;
 
     vector<Edge> edges;
 
-    for (int i = 0; i < V; ++i) {
-        for (int j = 0; j < V; ++j) {
+    for (int i = 0; i < V; ++i)
+    {
+        for (int j = 0; j < V; ++j)
+        {
             int weight;
             cin >> weight;
             if (weight != 0) {
